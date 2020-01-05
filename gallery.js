af07b5cd1313041bc09da0a7ee8c75fbde343fca
ricardopresto <ricardopresto5@gmail.com>
2020-01-05 15:21:49 +0100

    basic full-size preview working

--- gallery.js
+++ gallery.js
@@ -9,6 +9,15 @@ let images = [
   "ivy_edit8"
 ];
 
+let items = [];
+
+class GalleryItem {
+  constructor(fileName, orientation) {
+    this.fileName = fileName;
+    this.orientation = orientation;
+  }
+}
+
 let thumbsize = 200;
 let container = document.getElementById("container");
 let frameContainer = document.getElementById("frameContainer");
@@ -16,13 +25,21 @@ let frameContainer = document.getElementById("frameContainer");
 images.forEach(image => {
   let pic = document.createElement("img");
   pic.src = `./images/thumbs/${image}_th.jpg`;
-  pic.id = `${image}`;
+  pic.id = `${images.indexOf(image)}`;
+  pic.style.padding = "10px";
   pic.addEventListener("click", e => imageClick(e));
+  let newItem = new GalleryItem();
+  newItem.fileName = image;
   pic.onload = function() {
-    this.naturalWidth > this.naturalHeight
-      ? (this.style.width = `${thumbsize}px`)
-      : (this.style.height = `${thumbsize}px`);
+    if (this.naturalWidth > this.naturalHeight) {
+      this.style.width = `${thumbsize}px`;
+      newItem.orientation = "landscape";
+    } else {
+      this.style.height = `${thumbsize}px`;
+      newItem.orientation = "portrait";
+    }
   };
+  items.push(newItem);
   container.appendChild(pic);
 });
 
@@ -32,11 +49,18 @@ frameContainer.addEventListener("click", function() {
 });
 
 function imageClick(e) {
+  let id = e.srcElement.id;
   frameContainer.style.visibility = "visible";
   let frame = document.createElement("div");
   frame.id = "frame";
+  window.innerWidth > window.innerHeight
+    ? (frame.style.height = frame.style.width = `${window.innerHeight}px`)
+    : (frame.style.width = frame.style.height = `${window.innerWidth}px`);
   let pic = document.createElement("img");
-  pic.src = `./images/${e.srcElement.id}.jpg`;
+  pic.src = `./images/${items[id].fileName}.jpg`;
+  items[id].orientation == "landscape"
+    ? (pic.style.width = "100%")
+    : (pic.style.height = "100%");
   frame.appendChild(pic);
   frameContainer.appendChild(frame);
 }
